18b44195b5847a312fcb16ba4b00a071df8f22d8
Filipe Mattos <filipe14mattos@gmail.com>
2024-07-15 20:57:30 -0300

    add save user functionality

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,29 +1,33 @@
 const User = require('../models/User');
+const bcrypt = require("bcrypt");
 
 module.exports = class UserController {
   static async create(req, res) {
-    const user = {
-      name: req.body.name,
-      email: req.body.email,
-      phone: req.body.phone,
-      password: req.body.password,
-      confirmPassword: req.body.confirmPassword,
-    }
+    const {name, email, phone, password, confirmPassword} = req.body;
 
     // validation
-    if(!user.name || !user.email || !user.phone || !user.password || !user.confirmPassword) {
+    if(!name || !email || !phone || !password || !confirmPassword) {
       return res.status(422).json({message: 'Campos obrigatorios nao preenchidos'});
     }
     //Check password
-    if(user.password !== user.confirmPassword) {
+    if(password !== confirmPassword) {
       return res.status(422).json({message: 'A senha e a confirmação de senha precisam ser iguais!'});
     }
     //Check if user already exist
-    const userExists = await User.findOne({ raw: true, where: {email: user.email}})
+    const userExists = await User.findOne({ raw: true, where: {email: email}})
     if(userExists) {
       return res.status(422).json({message: 'O email ja esta cadastrado'});
     }
 
-    res.status(201).json({message: 'User registered'});
+    //Create Password
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    //Create User
+    const user = new User({name, email, phone, password: passwordHash})
+
+    const newUser = await user.save()
+      .then((result) => res.status(201).json({message: 'Usuário Cadastrado', data: result}))
+      .catch(err => console.log(err));
   }
 }
